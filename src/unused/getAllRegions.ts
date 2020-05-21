ab072d3328d008adffbc6460d18a3a978610f630
runar <runar@tuta.io>
2020-05-21 14:33:26 +0200

    move unused getAllRegions

--- src/unused/getAllRegions.ts
+++ src/unused/getAllRegions.ts
@@ -1,13 +1,13 @@
 import {
   journeys as flixJourneys,
   Station,
-  regions as flixRegions
+  regions as flixRegions,
 } from "flix";
-import { writeJsonToFile } from "./misc/helper";
+import { writeJsonToFile } from "../misc/helper";
 const berlinRegion: Station = { type: "region", id: "88" };
 const gorlitzRegion: Station = {
   type: "region",
-  id: "3408"
+  id: "3408",
 };
 
 console.log("hi");
@@ -23,7 +23,7 @@ const journeysToGorlitz = async () => {
 const getAllRegions = () => {
   const regionStream = flixRegions.all();
   const regions = [];
-  regionStream.on("data", region => {
+  regionStream.on("data", (region) => {
     regions.push(region);
   });
 
