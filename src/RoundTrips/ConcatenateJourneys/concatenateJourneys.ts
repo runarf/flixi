1f905db8f52b6671615c5c6207c26bbc2ba117cc
runar <runar@tuta.io>
2020-05-21 14:26:27 +0200

    Move function to file

--- src/RoundTrips/ConcatenateJourneys/concatenateJourneys.ts
+++ src/RoundTrips/ConcatenateJourneys/concatenateJourneys.ts
@@ -1,5 +1,9 @@
-import { Journey, Station, Leg } from "flix";
+import { Journey, Leg } from "flix";
 import { getSameJourneysWithDifferentStops } from "./getSameJourneysWithDifferentStops";
+import {
+  JourneyStationsAndCheapestPrice,
+  getJourneyStationsAndCheapestPrice,
+} from "./getJourneyStationsAndCheapestPrice";
 
 export const concatenateSimilarJourneys = (
   journeys: Journey[],
@@ -54,60 +58,4 @@ const getConcatenatedJourneys = (
     }
   );
   return concatenatedJourneys;
-};
-
-interface JourneyStationsAndCheapestPrice {
-  stations: Station[];
-  cheapestPrice: number;
-}
-
-const getJourneyStationsAndCheapestPrice = (
-  sameJourneysFromDifferentStations: Journey[],
-  isGoingThere: boolean
-): JourneyStationsAndCheapestPrice => {
-  const journeyStationsAndCheapestPrice = sameJourneysFromDifferentStations.reduce<
-    JourneyStationsAndCheapestPrice
-  >(
-    (journeyStationsAndCheapestPrice, journey) => {
-      const { legs } = journey;
-
-      const currentJourneyStation: Station = (isGoingThere
-        ? legs[0].origin
-        : legs[legs.length - 1].destination) as Station;
-
-      const stationIsAlreadyInListOfStations = journeyStationsAndCheapestPrice.stations.some(
-        (station) =>
-          station.name === currentJourneyStation.name &&
-          station.id === currentJourneyStation.id
-      );
-
-      const stationsForJourney = stationIsAlreadyInListOfStations
-        ? [...journeyStationsAndCheapestPrice.stations]
-        : [
-            ...journeyStationsAndCheapestPrice.stations,
-            currentJourneyStation,
-          ];
-
-      const journeyPrice = journey.price.amount;
-
-      const currentCheapestPrice =
-        journeyStationsAndCheapestPrice.cheapestPrice;
-
-      const currentJourneyIsTheCheapestSoFar =
-        currentCheapestPrice === 0 ||
-        journeyPrice < currentCheapestPrice;
-
-      const cheapestPrice = currentJourneyIsTheCheapestSoFar
-        ? journeyPrice
-        : currentCheapestPrice;
-
-      return {
-        stations: stationsForJourney,
-        cheapestPrice,
-      };
-    },
-    { stations: [], cheapestPrice: 0 }
-  );
-
-  return journeyStationsAndCheapestPrice;
 };
---
+++ src/RoundTrips/ConcatenateJourneys/getJourneyStationsAndCheapestPrice.ts
@@ -0,0 +1,46 @@
+import { Journey, Station } from "flix";
+export const getJourneyStationsAndCheapestPrice = (
+  sameJourneysFromDifferentStations: Journey[],
+  isGoingThere: boolean
+): JourneyStationsAndCheapestPrice => {
+  const journeyStationsAndCheapestPrice = sameJourneysFromDifferentStations.reduce<
+    JourneyStationsAndCheapestPrice
+  >(
+    (journeyStationsAndCheapestPrice, journey) => {
+      const { legs } = journey;
+      const currentJourneyStation: Station = (isGoingThere
+        ? legs[0].origin
+        : legs[legs.length - 1].destination) as Station;
+      const stationIsAlreadyInListOfStations = journeyStationsAndCheapestPrice.stations.some(
+        (station) =>
+          station.name === currentJourneyStation.name &&
+          station.id === currentJourneyStation.id
+      );
+      const stationsForJourney = stationIsAlreadyInListOfStations
+        ? [...journeyStationsAndCheapestPrice.stations]
+        : [
+            ...journeyStationsAndCheapestPrice.stations,
+            currentJourneyStation,
+          ];
+      const journeyPrice = journey.price.amount;
+      const currentCheapestPrice =
+        journeyStationsAndCheapestPrice.cheapestPrice;
+      const currentJourneyIsTheCheapestSoFar =
+        currentCheapestPrice === 0 ||
+        journeyPrice < currentCheapestPrice;
+      const cheapestPrice = currentJourneyIsTheCheapestSoFar
+        ? journeyPrice
+        : currentCheapestPrice;
+      return {
+        stations: stationsForJourney,
+        cheapestPrice,
+      };
+    },
+    { stations: [], cheapestPrice: 0 }
+  );
+  return journeyStationsAndCheapestPrice;
+};
+export interface JourneyStationsAndCheapestPrice {
+  stations: Station[];
+  cheapestPrice: number;
+}
